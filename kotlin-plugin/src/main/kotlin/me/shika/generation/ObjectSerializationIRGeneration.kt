53806e83cf77c074868394d88993b22e6063e1a4
Andrei Shikov <andreiishikov@gmail.com>
2021-07-20 16:00:14 +0100

    Fix IR object generation

--- kotlin-plugin/src/main/kotlin/me/shika/generation/ObjectSerializationIRGeneration.kt
+++ kotlin-plugin/src/main/kotlin/me/shika/generation/ObjectSerializationIRGeneration.kt
@@ -2,9 +2,9 @@ package me.shika.generation
 
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
+import org.jetbrains.kotlin.backend.common.ir.copyTo
 import org.jetbrains.kotlin.backend.common.lower.DeclarationIrBuilder
 import org.jetbrains.kotlin.descriptors.DescriptorVisibilities
-import org.jetbrains.kotlin.descriptors.Visibilities
 import org.jetbrains.kotlin.ir.IrElement
 import org.jetbrains.kotlin.ir.builders.declarations.addFunction
 import org.jetbrains.kotlin.ir.builders.irBlockBody
@@ -16,7 +16,6 @@ import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
 import org.jetbrains.kotlin.ir.visitors.IrElementVisitorVoid
 import org.jetbrains.kotlin.ir.visitors.acceptChildrenVoid
 import org.jetbrains.kotlin.ir.visitors.acceptVoid
-import org.jetbrains.kotlin.utils.addToStdlib.firstIsInstanceOrNull
 
 private object ObjectSerializationOrigin : IrDeclarationOriginImpl("object-serialization-fix", isSynthetic = true)
 
@@ -40,6 +39,9 @@ class ObjectSerializationIrGeneration : IrGenerationExtension {
             visibility = DescriptorVisibilities.PUBLIC
             origin = ObjectSerializationOrigin
         }.also { function ->
+            // Ensure it is not static
+            function.dispatchReceiverParameter = cls.thisReceiver?.copyTo(function)
+
             function.body = DeclarationIrBuilder(context, function.symbol, function.startOffset, function.endOffset)
                 .irBlockBody {
                     +irReturn(irGetObject(cls.symbol))
