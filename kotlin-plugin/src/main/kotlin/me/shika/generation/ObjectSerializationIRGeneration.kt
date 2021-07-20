4e5b9a79e13b36a157cf7ef74078ebd9d02a70f9
Nick Kleban <n.v.kleban@intergiro.tech>
2021-07-20 14:18:17 +0300

    Add kotlin 1.5 support

--- kotlin-plugin/src/main/kotlin/me/shika/generation/ObjectSerializationIRGeneration.kt
+++ kotlin-plugin/src/main/kotlin/me/shika/generation/ObjectSerializationIRGeneration.kt
@@ -3,6 +3,7 @@ package me.shika.generation
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.backend.common.lower.DeclarationIrBuilder
+import org.jetbrains.kotlin.descriptors.DescriptorVisibilities
 import org.jetbrains.kotlin.descriptors.Visibilities
 import org.jetbrains.kotlin.ir.IrElement
 import org.jetbrains.kotlin.ir.builders.declarations.addFunction
@@ -15,25 +16,28 @@ import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
 import org.jetbrains.kotlin.ir.visitors.IrElementVisitorVoid
 import org.jetbrains.kotlin.ir.visitors.acceptChildrenVoid
 import org.jetbrains.kotlin.ir.visitors.acceptVoid
+import org.jetbrains.kotlin.utils.addToStdlib.firstIsInstanceOrNull
 
 private object ObjectSerializationOrigin : IrDeclarationOriginImpl("object-serialization-fix", isSynthetic = true)
 
 class ObjectSerializationIrGeneration : IrGenerationExtension {
     override fun generate(moduleFragment: IrModuleFragment, pluginContext: IrPluginContext) {
-        moduleFragment.acceptVoid(
-            recursiveIrClassVisitor {
-                if (it.descriptor.needSerializableFix()) {
-                    fixSerializable(it, pluginContext)
+        if (moduleFragment.hasJvmPlatform()) {
+            moduleFragment.acceptVoid(
+                recursiveIrClassVisitor {
+                    if (it.needSerializableFix()) {
+                        fixSerializable(it, pluginContext)
+                    }
                 }
-            }
-        )
+            )
+        }
     }
 
     private fun fixSerializable(cls: IrClass, context: IrPluginContext) {
         cls.addFunction {
             name = SERIALIZABLE_READ
             returnType = context.irBuiltIns.anyNType
-            visibility = Visibilities.PUBLIC
+            visibility = DescriptorVisibilities.PUBLIC
             origin = ObjectSerializationOrigin
         }.also { function ->
             function.body = DeclarationIrBuilder(context, function.symbol, function.startOffset, function.endOffset)
--- kotlin-plugin/src/main/kotlin/me/shika/generation/utils.kt
+++ kotlin-plugin/src/main/kotlin/me/shika/generation/utils.kt
@@ -1,7 +1,14 @@
 package me.shika.generation
 
+import org.jetbrains.kotlin.backend.wasm.ir2wasm.allSuperInterfaces
 import org.jetbrains.kotlin.descriptors.ClassDescriptor
 import org.jetbrains.kotlin.incremental.components.NoLookupLocation
+import org.jetbrains.kotlin.ir.declarations.IrClass
+import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
+import org.jetbrains.kotlin.ir.util.fqNameWhenAvailable
+import org.jetbrains.kotlin.ir.util.functions
+import org.jetbrains.kotlin.ir.util.getAllSuperclasses
+import org.jetbrains.kotlin.ir.util.isObject
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.platform.has
@@ -26,6 +33,27 @@ fun ClassDescriptor.hasReadMethod() =
 fun ClassDescriptor.isSerializable(): Boolean =
     getSuperInterfaces().any { it.fqNameSafe == SERIALIZABLE_FQ_NAME || it.isSerializable() }
         || getSuperClassNotAny()?.isSerializable() == true
+
+fun IrClass.needSerializableFix(): Boolean {
+    return isObject && isSerializable() && !hasReadMethod()
+}
+
+fun IrClass.isSerializable(): Boolean {
+    return allSuperInterfaces()
+        .any { it.fqNameWhenAvailable == SERIALIZABLE_FQ_NAME || it.isSerializable() }
+        || getAllSuperclasses().any { it.isSerializable() }
+}
+
+fun IrClass.hasReadMethod(): Boolean {
+    return functions.any { it.name == SERIALIZABLE_READ && it.valueParameters.isEmpty() }
+}
+
+fun IrModuleFragment.hasJvmPlatform(): Boolean {
+    return descriptor
+        .platform
+        ?.has<JvmPlatform>()
+        ?: false
+}
 
 val SERIALIZABLE_READ = Name.identifier("readResolve")
 val SERIALIZABLE_FQ_NAME = FqName(Serializable::class.java.name)
