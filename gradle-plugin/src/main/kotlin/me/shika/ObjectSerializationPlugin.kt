d1a488463946199da1c6d964ce89a0590efcba04
Andrei Shikov <andreiishikov@gmail.com>
2021-11-16 23:16:20 +0000

    Merge plugin and subplugin to conform 1.6 api

--- gradle-plugin/src/main/kotlin/me/shika/ObjectSerializationPlugin.kt
+++ gradle-plugin/src/main/kotlin/me/shika/ObjectSerializationPlugin.kt
@@ -1,15 +1,51 @@
 package me.shika
 
-import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.internal.provider.DefaultListProperty
+import org.gradle.api.provider.Provider
+import org.jetbrains.kotlin.gradle.plugin.KotlinCompilation
+import org.jetbrains.kotlin.gradle.plugin.KotlinCompilerPluginSupportPlugin
+import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType
+import org.jetbrains.kotlin.gradle.plugin.SubpluginArtifact
+import org.jetbrains.kotlin.gradle.plugin.SubpluginOption
 
-class ObjectSerializationPlugin : Plugin<Project> {
-    override fun apply(project: Project) {
-        project.extensions.create(
+class ObjectSerializationPlugin : KotlinCompilerPluginSupportPlugin {
+    override fun apply(target: Project) {
+        target.extensions.create(
             "objectSerialization",
             ObjectSerializationExtension::class.java
         )
     }
+
+    override fun applyToCompilation(kotlinCompilation: KotlinCompilation<*>): Provider<List<SubpluginOption>> {
+        val project = kotlinCompilation.target.project
+        val extension = project.extensions.findByType(ObjectSerializationExtension::class.java) ?: ObjectSerializationExtension()
+
+        return DefaultListProperty(SubpluginOption::class.java).apply {
+            add(
+                SubpluginOption(
+                    key = "enabled",
+                    value = extension.enabled.toString()
+                )
+            )
+        }
+    }
+
+    override fun getCompilerPluginId(): String = "object-serialization-fix"
+
+    override fun getPluginArtifact(): SubpluginArtifact =
+        SubpluginArtifact(
+            groupId = "me.shika",
+            artifactId = "kotlin-object-java-serialization",
+            version = "1.2.0"
+        )
+
+    override fun isApplicable(kotlinCompilation: KotlinCompilation<*>): Boolean =
+        kotlinCompilation.platformType in setOf(
+            KotlinPlatformType.jvm,
+            KotlinPlatformType.androidJvm,
+            KotlinPlatformType.common
+        )
 }
 
 open class ObjectSerializationExtension {
